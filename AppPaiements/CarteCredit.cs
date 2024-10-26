71d0459258098b6f6f449beff3b9bf310bd292bc
Ali Djidi <300140112@monboreal.ca>
2024-10-26 00:03:29 -0400

    ajout

---
+++ AppPaiements/CarteCredit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+   
+    
+        internal class CarteCredit : Paiement
+        {
+
+            public int NumeroCarte { get; set; }
+
+
+            public CarteCredit(int numeroCarte, double montant, string description) : base(montant, description)
+            {
+                NumeroCarte = numeroCarte;
+            }
+
+            public override void AfficherDetails()
+            {
+                Console.WriteLine($"le montant est {Montant}, le numéro de la carte est {NumeroCarte} et la description est {Description}.");
+            }
+
+        }
+    
+}
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit carteCredit1 = new CarteCredit(27596597, 10000000, "Tchad");
+        CarteCredit carteCredit2 = new CarteCredit(30216560, 1628173, "boss");
+        CarteCredit carteCredit3 = new CarteCredit(182413, 127500, "juge");
+        List<CarteCredit> CarteCredits = new List<CarteCredit> { carteCredit1, carteCredit2, carteCredit3 };
+        Console.WriteLine("les cartes de credits sont :");
+        foreach (var CarteCredit in CarteCredits)
+        {
+            CarteCredit.AfficherDetails();
+        }
+
     }
-} 
+}
