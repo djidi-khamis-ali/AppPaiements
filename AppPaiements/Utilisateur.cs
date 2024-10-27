24838c1744c5613e628e1825fc2bc39374cb2fab
Ali Djidi <300140112@monboreal.ca>
2024-10-26 23:38:42 -0400

    ajout

---
+++ AppPaiements/Utilisateur.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur
+    { 
+        public string Nom { get; set; }
+        public List<Paiement> Paiements { get; set; }
+
+        
+
+
+        public Utilisateur(string nom)
+        {
+            Nom = nom;
+            Paiements = new List<Paiement>();
+        }
+
+        
+
+
+
+        public void AjouterPaiement(Paiement paiement)
+        {
+            Paiements.Add(paiement);
+        }
+        public void Afficherinfos()
+        {
+            Console.WriteLine($" {Nom} a  payer : ");
+            foreach (var paiement in Paiements)
+            {
+                paiement.AfficherDetails();
+            }
+        }
+    }
+}
+
